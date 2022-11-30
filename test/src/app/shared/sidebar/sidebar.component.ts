ff55e404ed334a34f17678a542b150431d7fa39f
nidyas <foboki111@hotmail.com>
2022-11-30 22:01:10 +0100

    testpush2

---
+++ test/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  public menuItems: any[] | undefined;
+  public isCollapsed = true;
+  ngOnInit(): void {
+  }
+
+}
